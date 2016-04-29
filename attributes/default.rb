78e5206557a96df79930d704ac6bb1c3a46600d7
kamalhakim <k.hakimzadeh@gmail.com>
2016-04-29 10:30:30 +0200

    Code generated by Karamel.

---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+# 'user' and 'group' define the unix user and group, respectively, 
+# that the experiment will be excecuted as.
+default[:riemann][:group] = "riemann"
+default[:riemann][:user] = "riemann"
+
---
+++ metadata.rb
@@ -0,0 +1,30 @@
+name             'riemann'
+maintainer       "riemann"
+maintainer_email "k.hakimzadeh@gmail.com"
+license          "Apache v2.0"
+description      'Installs/Configures/Runs riemann'
+version          "0.1"
+
+recipe            "riemann::install", "Experiment setup for riemann"
+recipe            "riemann::experiment",  "configFile=; Experiment name: experiment"
+
+
+depends "kagent"
+
+
+
+%w{ ubuntu debian rhel centos }.each do |os|
+  supports os
+end
+
+
+
+attribute "riemann/group",
+:description => "group parameter value",
+:type => "string"
+
+attribute "riemann/user",
+:description => "user parameter value",
+:type => "string"
+
+

335517d2b51a07ea50807ab2788596900961e2e4
stornqvist <basti94a@gmail.com>
2016-05-03 15:16:39 +0200

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -11,13 +11,9 @@ default[:riemann][:download][:version]   = '0.2.10'
 
 default[:riemann][:config][:bind]        =   '0.0.0.0'
 default[:riemann][:config][:port]        =   '5555'
-#default[:riemann][:config][:ws-port]     =   '5556'     
+default[:riemann][:config]['ws-port']     =   '5556'     
 default[:riemann][:config][:userfile]    =
                   '/usr/local/riemann/etc/user.config'
-
-#default[:riemann][:system][:user]        =   'riemann'
-#default[:riemann][:system][:group]       =   'riemann'
-#default[:riemann][:system][:home_dir]    =   '/usr/local/riemann'
 
 default[:java][:jdk_version] = '8'
 default[:java][:install_flavor] = 'oracle'

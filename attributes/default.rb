c008bd3990816477b2ac5d5a85d56f4ed7eb471f
Sebastian Törnqvist <basti94a@gmail.com>
2016-05-31 15:28:56 +0200

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,9 +9,9 @@ default[:riemann][:download][:url]       = 'http://aphyr.com/riemann/'
 default[:riemann][:download][:checksum]  = 'ec697519e80781dc478983de04204760f2790da1715acfd233b35da071be5455'
 default[:riemann][:download][:version]   = '0.2.10'
 
-default[:riemann][:config][:bind]        =   '0.0.0.0'
-default[:riemann][:config][:port]        =   '5555'
-default[:riemann][:config]['ws-port']     =   '5556'     
+default[:riemann][:server][:ip]        =   '0.0.0.0'
+default[:riemann][:server][:port]        =   '5555'
+default[:riemann][:dash][:port]          =   '5556'     
 default[:riemann][:config][:userfile]    =
                   '/usr/local/riemann/etc/user.config'
 
@@ -19,6 +19,4 @@ default[:java][:jdk_version] = '8'
 default[:java][:install_flavor] = 'oracle'
 default[:java][:oracle][:accept_oracle_download_terms] = true
 
-default[:riemann][:server][:ip] = ''
-default[:riemann][:server][:port] = ''
 

aaba8a7d668de604d991904fb77f387a109145d2
Kamal Hakimzadeh <k.hakimzadeh@gmail.com>
2016-07-04 13:27:39 +0200

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,8 @@
 default.riemann.group = "riemann"
 default.riemann.user = "riemann"
 default.riemann.install_dir = "/usr/local/riemann"
-default.riemann.config.userfile    =   "#{node.riemann.install_dir}/etc/user.config"
+default.riemann.config_dir    =   "#{node.riemann.install_dir}/etc"
+default.riemann.config.userfile    =   "#{node.riemann.conf_dir}/user.config"
 
 default.riemann.download.url       = 'http://aphyr.com/riemann/'
 default.riemann.download.checksum  = 'ec697519e80781dc478983de04204760f2790da1715acfd233b35da071be5455'

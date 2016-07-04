c5f37682af8d3a0fba8f9e58f8246fcc47ee345e
Kamal Hakimzadeh <k.hakimzadeh@gmail.com>
2016-07-04 12:36:28 +0200

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,6 @@
 # that the experiment will be excecuted as.
 default.riemann.group = "riemann"
 default.riemann.user = "riemann"
-default.riemann.home_dir = "/usr/local/riemann"
 
 default.riemann.download.url       = 'http://aphyr.com/riemann/'
 default.riemann.download.checksum  = 'ec697519e80781dc478983de04204760f2790da1715acfd233b35da071be5455'
@@ -13,7 +12,7 @@ default.riemann.server.private_ips		 =   ['']
 default.riemann.server.bind        =   '0.0.0.0'
 default.riemann.server.port        =   '5555'
 default.riemann.dash.port          =   '5556'     
-default.riemann.config.userfile    =   '/usr/local/riemann/etc/user.config'
+default.riemann.config.userfile    =   "#{node.riemann.user}/etc/user.config"
 
 default.java.jdk_version = '7'
 default.java.install_flavor = 'oracle'

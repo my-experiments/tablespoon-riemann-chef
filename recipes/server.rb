6a0bed71d993e9ccfb857310dfe5f5242b0cd82c
Kamal Hakimzadeh <k.hakimzadeh@gmail.com>
2016-07-04 13:14:43 +0200

    Update server.rb

--- recipes/server.rb
+++ recipes/server.rb
@@ -12,41 +12,4 @@ TODO:
 #>
 =end
 
-conf_dir = ::File.join(
-  node.riemann.home_dir,
-  'etc'
-)
-
-include_recipe 'runit'
-include_recipe 'java'
-include_recipe 'ark'
-include_recipe 'riemann::infra'
-
-ark 'riemann' do
-  url "#{node.riemann.download.url}riemann-#{node.riemann.download.version}.tar.bz2"
-  version node.riemann.download.version
-  checksum node.riemann.download.checksum
-  owner node.riemann.user
-  home_dir node.riemann.home_dir
-  action :install
-end
-
-link '/etc/riemann' do
-  to conf_dir
-end
-
-template ::File.join(conf_dir, 'riemann.config') do
-  owner node.riemann.user
-  group node.riemann.group
-  source 'riemann.config.erb'
-  mode '0644'
-end
-
 runit_service 'riemann-server'
-
-file node.riemann.config.userfile do
-  owner node.riemann.user
-  group node.riemann.group
-  action :create_if_missing
-  mode '0644'
-end

3f94ce4cd43be00a982c06ab89b4345b8c4fb015
Kamal Hakimzadeh <k.hakimzadeh@gmail.com>
2016-07-04 13:23:48 +0200

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,7 +17,5 @@ default.riemann.dash.port          =   '5556'
 
 
 node.default.java.jdk_version  = 7
-default.java.install_flavor = 'oracle'
-default.java.oracle.accept_oracle_download_terms = true
 
 
